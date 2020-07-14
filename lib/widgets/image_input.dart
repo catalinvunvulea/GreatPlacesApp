d0ecec92ed7c7cf94937c57b880f85f1b3cdb692
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-07-14 08:18:22 +0100

    Storing the image on the FIlesystem (on the device)

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,6 +2,9 @@ import 'dart:io'; //to use data stored on device
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as sysPath;
+
 
 class ImageInput extends StatefulWidget {
   ImageInput({Key key}) : super(key: key);
@@ -10,17 +13,28 @@ class ImageInput extends StatefulWidget {
   _ImageInputState createState() => _ImageInputState();
 }
 
-Future<void> _takePicture() async {
-  final picker =
-      ImagePicker(); // Provides an easy way to pick an image/video from the image library or take a picture/vide with the camera
-  final imageFile = await picker.getImage( //the palce when the picture will be stored once taken
-    source: ImageSource.camera,
-    maxWidth: 600,
-  );
-}
+
 
 class _ImageInputState extends State<ImageInput> {
   File _storedImage;
+
+  Future<void> _takePicture() async {
+  final picker =
+      ImagePicker(); // Provides an easy way to pick an image/video from the image library or take a picture/vide with the camera
+  final imageFile = await picker.getImage( //the palce when the picture will be stored once taken, for now; this is just the virtual memory
+    source: ImageSource.camera,
+    maxWidth: 600,
+  );
+ setState(() {
+   _storedImage = File(imageFile.path); //File(x.path) converts the picked file to a regular file
+ });
+  final appDirectory = await sysPath.getApplicationDocumentsDirectory(); //we set the path where the data should be saved
+  final fileName = path.basename(imageFile.path); //we store the name of the picture,
+  final savedImage = await File(imageFile.path).copy('${appDirectory.path}/$fileName'); //we copy the file (image in our case) on the device, first the location ${} and then name /$
+
+}
+
+
   @override
   Widget build(BuildContext context) {
     return Row(
