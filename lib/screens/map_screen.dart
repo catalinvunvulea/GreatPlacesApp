e8066b918d9db3d5ac78b7459f82d1c62967ee92
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-07-19 19:26:23 +0100

    Allowing Users to Pick a Location on the Map

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -19,11 +19,31 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng _pickedLocation;
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedLocation = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Map'),
+        actions: <Widget>[
+          if (widget.isSelecting)
+            IconButton(
+              icon: Icon(Icons.check),
+              onPressed: _pickedLocation == null
+                  ? null
+                  : () {
+                      Navigator.of(context).pop(
+                          _pickedLocation); //we return the pickedLocaiton when dismissing the screen
+                    },
+            )
+        ],
       ),
       body: GoogleMap(
         //GoofleMap assumes the width and height of the parrent widget
@@ -33,7 +53,17 @@ class _MapScreenState extends State<MapScreen> {
             widget.initialLocation.longitude,
           ),
           zoom: 16,
-         ),
+        ),
+        onTap: widget.isSelecting ? _selectLocation : null,
+        markers: _pickedLocation ==
+                null //markers returns a SET = similar to Map?dicitonary withouth a key, so like an array but won't add the same value twice (can't have duplications)
+            ? null
+            : {
+                Marker(
+                    // is a maker on the map
+                    markerId: MarkerId('m1'),
+                    position: _pickedLocation),
+              },
       ),
     );
   }
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../helpers/location_helper.dart';
 import '../screens/map_screen.dart';
@@ -28,7 +29,7 @@ class _LocationInputState extends State<LocationInput> {
   }
 
   Future<void> _selectOnMap() async {
-    final selectedLocation = await Navigator.of(context).push(
+    final  selectedLocation = await Navigator.of(context).push<LatLng>(//.push<LatLng> this tells us that once the pushed screen is dimissed, we should get back a LatLng
       MaterialPageRoute(
         fullscreenDialog: true, //to have a slightly different look when loading the page, and instead of back button will have an x in the bar
         builder: (ctx) => MapScreen(
@@ -40,6 +41,7 @@ class _LocationInputState extends State<LocationInput> {
     if (selectedLocation == null) {
       return;
     }
+    print(selectedLocation);
     //....
   }
 
