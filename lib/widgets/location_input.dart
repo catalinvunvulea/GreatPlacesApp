195cc6d0fadeaaff163a78fe8b26e09c96479193
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-07-19 19:05:03 +0100

    Rendering a Dynamic Map (via Google Maps)

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 
 import '../helpers/location_helper.dart';
+import '../screens/map_screen.dart';
 
 class LocationInput extends StatefulWidget {
   LocationInput({Key key}) : super(key: key);
@@ -24,6 +25,22 @@ class _LocationInputState extends State<LocationInput> {
     setState(() {
       _previewImageUrl = staticMapImageUrl;
     });
+  }
+
+  Future<void> _selectOnMap() async {
+    final selectedLocation = await Navigator.of(context).push(
+      MaterialPageRoute(
+        fullscreenDialog: true, //to have a slightly different look when loading the page, and instead of back button will have an x in the bar
+        builder: (ctx) => MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+
+    if (selectedLocation == null) {
+      return;
+    }
+    //....
   }
 
   @override
@@ -63,7 +80,7 @@ class _LocationInputState extends State<LocationInput> {
             ),
             SizedBox(width: 10),
             FlatButton.icon(
-              onPressed: null,
+              onPressed: _selectOnMap,
               icon: Icon(Icons.map),
               label: Text('Select on Map'),
               textColor: Theme.of(context).primaryColor,
